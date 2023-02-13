183ff5c62c2a95c77533cf1d592e9107fa12898b
NishantFuturism <nishanti@futurismtechnologies.com>
2023-02-13 19:23:09 +0530

    test case added

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -8,7 +8,22 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react-native';
+import debugDeep from '@testing-library/react-native/build/helpers/debugDeep';
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// test("Testing first time",() => {
+//    expect(true).toBe(false);
+// }) 
+
+
+it("Welcome Screen from App.js", () => {
+  
+
+   const screen =  render(<App />)
+   screen.getByText("Hi There");
+   
+
+})
