354f41294d3c0a217e935b46a97577d57c97fed0
NishantFuturism <nishanti@futurismtechnologies.com>
2023-03-01 15:52:20 +0530

    ui changed

--- src/components/ProductsListItem.js
+++ src/components/ProductsListItem.js
@@ -1,8 +1,22 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {Text,View,StyleSheet, FlatList} from 'react-native';
+import {Text,View,StyleSheet, FlatList, Dimensions, Image} from 'react-native';
 const ProductListItem = ({item,index}) => {
     return(
-        <Text style={{width : 200,height : 100}}>{index + 1}. I am {item.name} and {item.status}</Text>
+        <View style={{
+            width : Dimensions.get('window').width - 20,
+            height :  200,
+            justifyContent : 'space-evenly',
+            alignItems : 'center',
+            // flexDirection : 'row',
+        }}>
+                   <Image
+        style={{
+            width : 100,height : 100,resizeMode : 'contain'
+        }}
+        source={require('../../assets/bootsplash_logo_original.png')}
+      />
+            <Text>{index + 1}. I am {item.name} and {item.status}</Text>
+        </View>
     )
 }
 
