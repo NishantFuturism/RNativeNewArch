7d30b3984c5a03220951f62611101ec9d3f3fce6
NishantFuturism <nishanti@futurismtechnologies.com>
2023-04-06 11:44:48 +0530

    added

--- src/reusableMapComponents/CreateGeoFence.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {View,Text} from 'react-native';
-
-const CreateGeoFence = () => {
-    return(
-       <Text>This is CreateGeoFence Screen</Text>     
-    )
-}
-export default CreateGeoFence;
