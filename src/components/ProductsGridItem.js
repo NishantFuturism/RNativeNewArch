3738eaf19c58a62d89d76245ebeb86eda32d4c43
NishantFuturism <nishanti@futurismtechnologies.com>
2023-04-07 04:10:46 +0530

    changes added

--- src/components/ProductsGridItem.js
+++ src/components/ProductsGridItem.js
@@ -4,7 +4,7 @@ import Colors from "../constants/Colors";
 const ProductsGridItem = ({item,index}) => {
      return (
         <View style={{justifyContent : 'center', alignItems : "center" ,width : 150, height : 150,borderColor : Colors.dividerColor,borderRadius : 10,borderWidth : 0.5,marginHorizontal : 3,backgroundColor : 'black'}}>
-                <Text style={{color : 'white'}}>{item.name}</Text>
+                <Text style={{color : 'white'}}>{index + 1}.{item.name}</Text>
         </View>
      )
 }
