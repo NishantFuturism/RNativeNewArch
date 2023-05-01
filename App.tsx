adb4a06b2fbdea05cfc22c0dffbd5209e4aec15d
NishantFuturism <nishanti@futurismtechnologies.com>
2023-05-01 16:49:35 +0530

    carousel added

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 // import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,7 +15,8 @@ import {
   Text,
   useColorScheme,
   View,
-  Dimensions
+  Dimensions,
+  Image
 } from 'react-native';
 
 import {
@@ -30,47 +31,74 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const width = Dimensions.get('window').width;
 
+  const [images, setImages] = useState([]);
+
+  
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   useEffect(() => {
-      RNBootSplash.hide({ fade: true, duration: 5000 });
-      console.log("Bootsplash has been hidden successfully");
+    RNBootSplash.hide({ fade: true, duration: 5000 });
+    console.log("Bootsplash has been hidden successfully");
   }, []);
 
+  useEffect(() => {
+    fetch("http://192.168.43.194:4242/storage/getFiles").then(async res => {
+      let response = await res.json();
+      return response.files;
+    }).then(final => {
+      setImages(final.filter(item => item.mimeType !== 'application/pdf'));
+    })
+  }, [])
+
+
   return (
-    
-    
-        <View style={{ flex: 1 }}>
-            <Carousel
-                loop
-                width={width}
-                height={width / 2}
-                autoPlay={true}
-                data={[...new Array(6).keys()]}
-                scrollAnimationDuration={1000}
-                panGestureHandlerProps={{
-                  activeOffsetX: [-10, 10],
-                }}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index }) => (
-                    <View
-                        style={{
-                            flex: 1,
-                            borderWidth: 1,
-                            justifyContent: 'center',
-                        }}
-                    >
-                        <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                            {index}
-                        </Text>
-                    </View>
-                )}
+
+
+    <View style={{ flex: 1,justifyContent : 'center',alignItems : 'center' }}>
+      <Carousel
+        loop
+        width={width}
+        height={width / 2}
+        autoPlay={true}
+        data={images}
+        style={{flex : 1,alignItems : 'center',justifyContent : 'center'}}
+        scrollAnimationDuration={1000}
+        panGestureHandlerProps={{
+          activeOffsetX: [-10, 10],
+        }}
+        onSnapToItem={(index) => console.log('current index:', index)}
+        renderItem={({ item,index }) => (
+          <View
+            style={{
+              flex: 1,
+              borderWidth: 1,
+              justifyContent: 'center',
+            }}
+          >
+            <>
+            {console.log("ITEMMEM",item)}
+            </>
+            <Image
+              style={{
+                width: Dimensions.get('window').width - 20,
+                height: Dimensions.get('window').height,
+                marginVertical: 30,
+                alignSelf: 'center',
+                // aspectRatio : 40/100
+              }}
+              source={{
+                uri: `http://192.168.43.194:4242/uploads/${item.name}`,
+              }}
             />
-        </View>
-  
-     
+          </View>
+        )}
+      />
+    </View>
+
+
   );
 }
 
