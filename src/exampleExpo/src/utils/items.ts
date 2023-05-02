c3a1b07e863a2dd0250ea4d27801c24b9bc718d0
NishantFuturism <nishanti@futurismtechnologies.com>
2023-05-02 17:29:26 +0530

    other reusable components for carousel are added

---
+++ src/exampleExpo/src/utils/items.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+
+
+
+export const fruitItems = [require("../../assets/fruit-0.png"), require("../../assets/fruit-1.png"), require("../../assets/fruit-2.png")];
+
+export const ImageItems = [
+  require("../../assets/carousel-0.jpg"),
+  require("../../assets/carousel-1.jpg"),
+  require("../../assets/carousel-2.jpg"),
+];
