614b3a2071480091a729428b761f68d1ffe9ddac
NishantFuturism <nishanti@futurismtechnologies.com>
2023-05-02 17:28:28 +0530

    carousel added successfully

--- App.tsx
+++ App.tsx
@@ -16,7 +16,8 @@ import {
   useColorScheme,
   View,
   Dimensions,
-  Image
+  Image,
+  Button
 } from 'react-native';
 
 import {
@@ -24,6 +25,9 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import RNBootSplash from "react-native-bootsplash";
 import Carousel from 'react-native-reanimated-carousel';
+import { TAnimationStyle } from './src/layouts/BaseLayout';
+import Animated, { interpolate, interpolateColor, useAnimatedStyle } from 'react-native-reanimated';
+import { SBItem } from './src/exampleExpo/src/components/SBItem';
 
 
 
@@ -32,12 +36,67 @@ function App(): JSX.Element {
   const width = Dimensions.get('window').width;
 
   const [images, setImages] = useState([]);
-
-  
+  const [loop,setLoop] = useState(false);
+  interface ItemProps {
+    index: number
+    animationValue: Animated.SharedValue<number>
+  }
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const CustomItem: React.FC<ItemProps> = ({ index, animationValue }) => {
+    const maskStyle = useAnimatedStyle(() => {
+      const backgroundColor = interpolateColor(
+        animationValue.value,
+        [-1, 0, 1],
+        ["transparent", "transparent", "transparent"],
+      );
+
+      return {
+        backgroundColor,
+      };
+    }, [animationValue]);
+
+    return (
+      <View style={{ flex: 1 }}>
+        <SBItem key={index} index={index} style={{ borderRadius: 0 }} />
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            {
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+            },
+            maskStyle,
+          ]}
+        />
+      </View>
+    );
+  };
+
+  const animationStyle: TAnimationStyle = React.useCallback(
+    (value: number) => {
+      "worklet";
+
+      const zIndex = interpolate(value, [-1, 0, 1], [10, 20, 30]);
+      const translateX = interpolate(
+        value,
+        [-2, 0, 1],
+        [-60, 0, 40],
+      );
+
+      return {
+        transform: [{ translateX }],
+        zIndex,
+      };
+    },
+    [],
+  );
 
   useEffect(() => {
     RNBootSplash.hide({ fade: true, duration: 5000 });
@@ -57,8 +116,8 @@ function App(): JSX.Element {
   return (
 
 
-    <View style={{ flex: 1,justifyContent : 'center',alignItems : 'center' }}>
-      <Carousel
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* <Carousel
         loop
         width={width}
         height={width / 2}
@@ -95,7 +154,91 @@ function App(): JSX.Element {
             />
           </View>
         )}
+      /> */}
+
+      <Carousel
+        loop={loop}
+        autoPlay={true}
+        style={{ width: Dimensions.get('window').width - 20, height: Dimensions.get('window').width - 20 }}
+        width={Dimensions.get('window').width - 20}
+        data={images}
+        // renderItem={({ index, animationValue }) => {
+        //   return (
+        //     <CustomItem
+        //       key={index}
+        //       index={index}
+        //       animationValue={animationValue}
+        //     />
+        //   );
+        // }}
+        renderItem={({ item, index, animationValue }) => {
+
+          const maskStyle = useAnimatedStyle(() => {
+            const backgroundColor = interpolateColor(
+              animationValue.value,
+              [-1, 0, 1],
+              ["transparent", "transparent", "transparent"],
+            );
+
+            return {
+              backgroundColor,
+            };
+          }, [animationValue]);
+
+          return (
+            <>
+              <View
+                key={index}
+                style={{
+                  flex: 1,
+                  borderWidth: 1,
+                  justifyContent: 'center',
+                }}
+              >
+                <>
+                </>
+                <Image
+                  style={{
+                    width: Dimensions.get('window').width - 20,
+                    height: Dimensions.get('window').height,
+                    marginVertical: 30,
+                    alignSelf: 'center',
+                    // aspectRatio : 40/100
+                  }}
+                  source={{
+                    uri: `http://192.168.43.194:4242/uploads/${item.name}`,
+                  }}
+                />
+              </View>
+              <Animated.View
+                pointerEvents="none"
+                style={[
+                  {
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                  },
+                  maskStyle,
+                ]}
+              />
+            </>
+          )
+        }
+        }
+        customAnimation={animationStyle}
+        scrollAnimationDuration={500}
       />
+      <Button
+        onPress={() => {
+          setLoop(!loop);
+        }}
+        title={loop ? 'Turn off autoplay' : 'Turn on autoplay'}
+        color={"red"}
+      />
+
+
     </View>
 
 
