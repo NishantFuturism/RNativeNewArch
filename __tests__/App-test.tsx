fee0cbc986ff59d9c6d9f54ec070492ba2dd159e
NishantFuturism <nishanti@futurismtechnologies.com>
2023-07-06 00:45:25 +0530

    code added

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,10 +5,140 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import { render, fireEvent } from '@testing-library/react-native';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+describe('CarouselComponent', () => {
+  test('renders without errors', () => {
+    render(<App />);
+    // Assert that the component renders without throwing any errors
+  });
+
+  test('displays initial active slide', () => {
+    const { getByTestId } = render(<App />);
+    const activeSlide = getByTestId('active-slide');
+    
+    // Assert that the active slide is the first slide by default
+    expect(activeSlide.props.slideIndex).toBe(0);
+  });
+
+  test('renders all slides', () => {
+    const { getAllByTestId } = render(<App />);
+    const slides = getAllByTestId('slide');
+    
+    // Assert that all slides are rendered
+    expect(slides.length).toBe(3); // Adjust the expected number of slides based on your component
+  });
+
+  test('moves to next slide on button click', () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+    const nextButton = getByTestId('next-button');
+    const slides = getAllByTestId('slide');
+
+    // Click the next button to move to the next slide
+    fireEvent.press(nextButton);
+
+    // Assert that the active slide has changed to the next slide
+    const activeSlide = getByTestId('active-slide');
+    expect(activeSlide.props.slideIndex).toBe(1); // Adjust the expected slide index based on your component
+  });
+
+  test('moves to previous slide on button click', () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+    const prevButton = getByTestId('prev-button');
+    const slides = getAllByTestId('slide');
+
+    // Click the previous button to move to the previous slide
+    fireEvent.press(prevButton);
+
+    // Assert that the active slide has changed to the previous slide
+    const activeSlide = getByTestId('active-slide');
+    expect(activeSlide.props.slideIndex).toBe(2); // Adjust the expected slide index based on your component
+  });
+
+  test('moves to specific slide on indicator click', () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+    const indicator = getByTestId('indicator');
+    const slides = getAllByTestId('slide');
+
+    // Click the indicator corresponding to a specific slide
+    fireEvent.press(indicator);
+
+    // Assert that the active slide has changed to the clicked slide
+    const activeSlide = getByTestId('active-slide');
+    expect(activeSlide.props.slideIndex).toBe(1); // Adjust the expected slide index based on your component
+  });
+
+  // Add more test cases to cover other aspects of the carousel component
+});
+
+describe('CarouselComponent2', () => {
+  test('handles edge case of no slides', () => {
+    const { getByText } = render(<App slides={[]} />);
+    
+    // Assert that a message or placeholder is displayed when there are no slides
+    expect(getByText('No slides to display')).toBeTruthy();
+  });
+
+  test('automatically transitions to next slide after a set interval', () => {
+    jest.useFakeTimers(); // Mock timers to control time-based events
+    const { getByTestId, getAllByTestId } = render(<App autoPlayDuration={5000} />);
+    const slides = getAllByTestId('slide');
+
+    // Advance timers to trigger the automatic slide transition
+    jest.advanceTimersByTime(5000);
+
+    // Assert that the active slide has changed to the next slide after the set interval
+    const activeSlide = getByTestId('active-slide');
+    expect(activeSlide.props.slideIndex).toBe(1); // Adjust the expected slide index based on your component
+  });
+
+  test('pauses auto-play when user interacts with the carousel', () => {
+    jest.useFakeTimers();
+    const { getByTestId, getAllByTestId } = render(<App autoPlayDuration={5000} />);
+    const nextButton = getByTestId('next-button');
+    const slides = getAllByTestId('slide');
+
+    // Simulate user interaction by clicking the next button
+    fireEvent.press(nextButton);
+
+    // Advance timers to check if the auto-play is paused
+    jest.advanceTimersByTime(5000);
+
+    // Assert that the active slide has not changed since the auto-play should be paused
+    const activeSlide = getByTestId('active-slide');
+    expect(activeSlide.props.slideIndex).toBe(0); // Assert that the active slide remains the same
+
+    // Resume auto-play by interacting with the carousel again
+    fireEvent.press(nextButton);
+
+    // Advance timers to check if the auto-play resumes after user interaction
+    jest.advanceTimersByTime(5000);
+
+    // Assert that the active slide has changed to the next slide after the resumed auto-play
+    expect(activeSlide.props.slideIndex).toBe(1); // Adjust the expected slide index based on your component
+  });
+
+  test('applies correct animations when transitioning between slides', () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+    const nextButton = getByTestId('next-button');
+    const slides = getAllByTestId('slide');
+
+    // Click the next button to transition to the next slide
+    fireEvent.press(nextButton);
+
+    // Assert that the active slide and the previous slide have the appropriate animation properties
+    const activeSlide = getByTestId('active-slide');
+    const previousSlide = getByTestId('previous-slide');
+    expect(activeSlide.props.style.transform).toContain('translateX(0%)');
+    expect(previousSlide.props.style.transform).toContain('translateX(-100%)');
+  });
+
+  // Add more test cases to cover other functionality and edge cases
 });
